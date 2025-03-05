62e048ea8268be850d3fb0d9cddde13505e20da1
Swan Htet Zaw <htetzawswan@gmail.com>
2025-03-05 14:46:16 +0800

    Complete prac 5 word occurrences exercise

---
+++ prac_05/word_occurrences.py
@@ -0,0 +1,19 @@
+"""
+Word Occurrences
+Estimate: 20 minutes
+Actual:   23 minutes
+"""
+
+text = input("Text: ")
+
+words = text.split()
+
+word_counts = {}
+
+for word in words:
+    word_counts[word] = word_counts.get(word, 0) + 1
+
+max_word_length = max(len(word) for word in word_counts)
+
+for word in sorted(word_counts):
+    print(f"{word:{max_word_length}} : {word_counts[word]}")
